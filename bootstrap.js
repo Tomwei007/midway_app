e308302fd56fdb9ee739c75a02e21a229b7493f8
欧阳 <7535404+ouyw@user.noreply.gitee.com>
2024-12-25 16:02:09 +0800

    update

--- bootstrap.js
+++ bootstrap.js
@@ -1,2 +1,9 @@
+// 获取框架
+const WebFramework = require('@midwayjs/web').Framework;
+// 初始化 web 框架并传入启动参数
+const web = new WebFramework().configure({
+  port: 7001,
+});
+
 const { Bootstrap } = require('@midwayjs/bootstrap');
-Bootstrap.run();
+Bootstrap.load(web).run();
